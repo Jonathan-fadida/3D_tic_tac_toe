7ae22771266a71a65f6daba8cff0a327109c418c
kinanhino <kinan.hino@e.braude.ac.il>
2023-09-22 20:16:23 +0300

    added changing value on 2d board + winner checking functionality

--- three_dem_tic.py
+++ three_dem_tic.py
@@ -4,8 +4,8 @@ from two_dem_tic import TwoDemBoard
 class ThreeDemBoard(TwoDemBoard):
     def __init__(self):
         super().__init__()
-        s1 = TwoDemBoard()
-        s1.init_board()
+        # s1 = TwoDemBoard()
+        # s1.init_board()
 
 
     def print_all(self):
